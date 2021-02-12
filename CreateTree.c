befb4d2e39ca67c49e8358d891bea7c283146661
utkarsh raj <manoharshastri273273@gmail.com>
2021-02-12 14:36:24 +0530

    Logic of Binary tree

--- CreateTree.c
+++ CreateTree.c
@@ -1,24 +1,92 @@
 #include "headers.h"
-#include "structures.h"
 #include "declerations.h"
-void** CreateTree()
+int*  CreateTree()
 {
-    int h,ndata;
-    int i=1;
-    Node **Tree;
+    int ndata;
+    int i;
+    int *Tree;
+    char ch;
     printf("Enter the height of Tree\n");
     scanf("%d",&h);
     printf("Enter the Data  of Node\n");
     scanf("%d",&ndata);
-    Tree=(Node**)malloc(sizeof(Node)*(2*h-1));
-    if(i==1)
+    Tree=(int*)malloc(sizeof(int)*(2*h-1));
+    for(i=1;i<(2*h-1);i++)
     {
-        (*(Tree+i))->data=ndata;
+        *(Tree+i)=0;
     }
-    (*(Tree+2*i))->data=ndata;
-    (*(Tree+2*i+1))->data=ndata;
-    printf("Enter the left node of %d\n",(*(Tree+2*i))->data);
-
+    for(i=1;i<(2*h-1);i++)
+    {
+        if(i==1)
+        {
+            *(Tree+i)=ndata;
+            
+            printf("Do you want left child of %d (y/n)\n",*(Tree+i));
+            getchar();
+            scanf("%c",&ch);
+            if(ch=='y')
+            {
+                printf("Enter the Data  of left Node\n");
+                scanf("%d",&ndata);
+                *(Tree+2*i)=ndata;
+            }
+            else
+            {
+                printf("Do you want Right child of %d (y/n)\n",*(Tree+i));
+                getchar();
+                scanf("%c",&ch);          
+                if(ch=='y')
+                {
+                    printf("Enter the Data  of Right Node\n");
+                    scanf("%d",&ndata);
+                    *(Tree+2*i+1)=ndata;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        
+        }
+        else
+        {
+            if(*(Tree+i)==0)
+            {
+                printf("Enter the Data  of New Node\n");
+                scanf("%d",&ndata);
+                *(Tree+i)=ndata;
+            }
+            printf("Do you want left child of %d (y/n)\n",*(Tree+i));
+            getchar();
+            scanf("%c",&ch);
+            
+            if(ch=='y')
+            {
+                printf("Enter the Data  of left child\n");
+                 scanf("%d",&ndata);
+                *(Tree+2*i)=ndata;
+            }
+            else
+            {
+                continue;
+            }
+            printf("Do you want Right child of %d (y/n)\n",*(Tree+i));
+            getchar();
+            scanf("%c",&ch);
+            
+            if(ch=='y')
+            {
+                printf("Enter the Data  of Right child\n");
+                scanf("%d",&ndata);
+                *(Tree+2*i+1)=ndata;
+            }
+            else
+            {
+                continue;
+            }
+        }
+      
+    }
     return Tree;
 
 }
