acad01aa2b0c1a444a000cc6cc45b49b2ccdb3a6
Sakshi Taneja <79049401+Sakshitaneja4@users.noreply.github.com>
2021-02-20 22:57:33 +0530

    Add files via upload

--- Exitfunc.c
+++ Exitfunc.c
@@ -2,16 +2,18 @@
 #include "declerations.h"
 void** ExitFunc(void **arg)
 {
-    char *stat;
+    char **stat;
     printf("%s:Begin\n",__FILE__);
-    stat=(char*)arg;
-    if(strncmp(stat,"Success",7)==0)
+
+    stat=(char**)arg;
+    if(strncmp(*stat,"Success",7)==0)
     {
         exit(EXIT_SUCCESS);
     }
-    else if(strncmp(stat,"Failure",7)==0)
+    if(strncmp(*stat,"Failure",7)==0)
     {
         exit(EXIT_FAILURE);
     }
+
     printf("%s:End\n",__FILE__);
 }
