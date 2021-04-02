e4101b3cad2160c935052c055e48d6c199d4c3db
root <root@localhost.localdomain>
2021-04-02 23:19:21 +0530

    Display function modified

--- DisplayTree.c
+++ DisplayTree.c
@@ -13,14 +13,26 @@ void** DisplayTree(void **arg)
         i++;
     }
     printf("\n");
+    i=1;   
+    while(i<idx)
+    {
+        printf("______");
+        i++;
+    }
+    printf("\n");
     i=1;
     printf("| %d |",(*(tree+i))->value);
     while(i<idx)
     {
         //printf("| %d ",(*(tree+i))->value);
-        printf(" %d |",(*(tree+2*i))->value);
-        printf(" %d |",(*(tree+(2*i+1)))->value);
+	if((*(tree+i))->lchild!=NULL||(*(tree+i))->rchild!=NULL)
+	{
+        	printf(" %d |",(*(tree+2*i))->value);
+        	printf(" %d |",(*(tree+(2*i+1)))->value);
+	}
         i++;
     }
+    printf("\n");
     printf("%s:End\n",__FILE__);
+    return (void**)tree;
 }
