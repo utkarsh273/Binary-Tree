36b3ad72fb77c20ba8edcf2d4923449ce3bc396b
utkarsh raj <manoharshastri273273@gmail.com>
2021-04-02 16:24:26 +0530

    Display Tree implemented

--- DisplayTree.c
+++ DisplayTree.c
@@ -3,5 +3,24 @@
 void** DisplayTree(void **arg)
 {
     printf("%s:Begin\n",__FILE__);
+    Node **tree;
+    int i;
+    tree=(Node**)arg;
+    i=1;   
+    while(i<idx)
+    {
+        printf("   %d  ",i);
+        i++;
+    }
+    printf("\n");
+    i=1;
+    printf("| %d |",(*(tree+i))->value);
+    while(i<idx)
+    {
+        //printf("| %d ",(*(tree+i))->value);
+        printf(" %d |",(*(tree+2*i))->value);
+        printf(" %d |",(*(tree+(2*i+1)))->value);
+        i++;
+    }
     printf("%s:End\n",__FILE__);
 }
