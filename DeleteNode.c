f23173d1026cc42ea09d66164a0699cf37c27da4
utkarsh raj <manoharshastri273273@gmail.com>
2021-04-05 18:37:25 +0530

    Delete Implementation started

--- DeleteNode.c
+++ DeleteNode.c
@@ -3,6 +3,23 @@
 void** DeleteNode(void **arg)
 {
     printf("%s:Begin\n",__FILE__);
+    int i,key,loc;
+    Node **tree;
+    Node *Tree;
+    tree=(Node**)arg;
+    printf("Enter the key to delete\n");
+    scanf("%d",&key);
+    i=1;
+    while((*(tree+i))->rchild!=NULL)
+    {
+        Tree=(*(tree+i))->rchild;
+        loc=i;
+        i++;
+    }
+    i=1;
+    while((*(tree+i++))->value!=key);
+    (*(tree+i))->value=Tree->value;
+    free((*(tree+loc))->rchild);
     printf("%s:End\n",__FILE__);
-
+    return (void**)tree;
 }
