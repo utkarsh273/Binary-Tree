7fd2f64a3657784c89de3f992c05f9f19b61f331
utkarsh raj <manoharshastri273273@gmail.com>
2021-04-06 09:14:08 +0530

    comments added

--- DeleteNode.c
+++ DeleteNode.c
@@ -9,6 +9,7 @@ void** DeleteNode(void **arg)
     tree=(Node**)arg;
     printf("Enter the key to delete\n");
     scanf("%d",&key);
+//-----------------------Finding deepest RightMost node-----------------------------------------//
     i=1;
     while((*(tree+i))->rchild!=NULL)
     {
@@ -16,15 +17,17 @@ void** DeleteNode(void **arg)
         loc=i;
         i++;
     }
+//---------------------------------------------------------------------------------------------//
+//-------------------Finding Node that we want to Delete---------------------------------------//
     i=1;
     while((*(tree+i))->value!=key)
     {
         lockey=i;
         i++;
     }
-    (*(tree+lockey+1))->value=Tree->value;
-    free((*(tree+loc))->rchild);
-    (*(tree+loc))->rchild=NULL;
+    (*(tree+lockey+1))->value=Tree->value;//since the location we got is one index before hte node that we want to delete so lockey+1 is used
+    free((*(tree+loc))->rchild);//deallocating RightMost Node 
+    (*(tree+loc))->rchild=NULL;//Deleting the RightMost Node
     printf("new lchild of %d :%d and Rchild:%d\n",(*(tree+lockey+1))->parent->value,(*(tree+lockey+1))->parent->lchild->value,(*(tree+lockey+1))->parent->rchild->value);
     printf("%s:End\n",__FILE__);
     return (void**)tree;
